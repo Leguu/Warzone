00efddd79b942426a725453845fcdff4413523c0
Khaled Al K <57811573+kkodmani@users.noreply.github.com>
2022-09-22 15:30:31 -0400

    Constructors and operators are written
    Just finished the necessary 3 things we need for each class, implementation with source will follow

--- src/Map/Map.h
+++ src/Map/Map.h
@@ -2,49 +2,86 @@
 #define WARZONE_MAP_H
 
 class Territory;
-// testing commit
 
 #include <string>
 #include <ostream>
+#include <vector>
 #include "../Player/Player.h"
+
+using std::string;
+using std::vector;
+
 
 class Continent {
 public:
-    const std::string name;
+    const string name;
     const int bonus;
+    vector<Territory *> ownedTerritories;
 
     // TODO: Calculate the owner
     Player *owner();
 
-    Continent(std::string name, int bonus);
+    // Default constructor
+    Continent();
+
+    // Constructor with necessary name and optional bonus
+    Continent(string name, int bonus = 0);
+
+    // Constructor with name, list of pointers to territories and an optional bonus
+    Continent(string name, vector<Territory *> territories, int bonus = 0);
+
+    // Copy constructor
+    Continent(const Continent &orgContinent);
+
+    // Assigment constructor
+    Continent &operator=(const Continent &continent);
+
+    // Insertion operator
+    friend std::ostream &operator<<(std::ostream &os, const Continent &continent);
+
+    // Destructor
+    virtual ~Continent();
 
 protected:
-    /// TODO list of references? also for Territory::adjacentTerritories
-    std::vector<Territory *> territories = {};
-
     friend class MapLoader;
 };
 
 class Territory {
 public:
     const int id = idIncrement++;
-    const std::string name;
-
+    const string name;
+    const string continentName;
     Player *owner = nullptr;
-    int armies = 10;
+    vector<Territory *> adjacentTerritories;
+    // int armies = 10;
 
-    explicit Territory(std::string name);
+    // Default constructor
+    Territory();
 
-    [[nodiscard]] std::string toString() const;
+    // Constructor with necessary parameters
+    Territory(int id, string name, string continentName);
 
+    // Copy constructor
+    Territory(const Territory &orgTerritory);
+
+    // Assignment operator
+    Territory &operator=(const Territory &territory);
+
+    // Insertion operator
     friend std::ostream &operator<<(std::ostream &os, const Territory &territory);
+
+    // Destructor
+    virtual ~Territory();
+
+    // To string method
+    [[nodiscard]] string toString() const;
+
 
 protected:
     /// Global variable for assigning territory ids.
     /// The ids for allTerritories need to be globally unique, so this can be static.
     static int idIncrement;
 
-    std::vector<Territory *> adjacentTerritories;
     /// Todo is this really necessary?
     Continent *continent = nullptr;
 
@@ -55,10 +92,25 @@ class Map {
 public:
     // TODO Map owns these pointers, they should be destructed properly.
     // Could probably be unique_ptrs to make that clear
+    // no unique pointers will be used
+    const string name;
     const std::vector<Continent *> continents;
     const std::vector<Territory *> allTerritories;
 
+    // Default Constructors
+    Map();
+
+    // Constructor with necessary parameters
     Map(std::vector<Continent *> continents, std::vector<Territory *> territories);
+
+    // Copy Constructor
+    Map(const Map &orgMap);
+
+    // Assignment operator
+    Map &operator=(const Map &map);
+
+    // Insertion operator
+    friend std::ostream &operator<<(std::ostream &os, const Map &map);
 
     /// Figure out whether every continents have an owner?
     inline bool allContinentsOwned() { return false; }
@@ -102,7 +154,7 @@ public:
     // - territories are adjacent to at least one thing
     // - territories have a continent
     // - continents have at least one territory
-    static std::unique_ptr<Map> importMap(const std::string &path) noexcept(false);
+    static std::unique_ptr<Map> importMap(const string &path) noexcept(false);
 };
 
 #endif //WARZONE_MAP_H
