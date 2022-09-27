ea01197e5a15b4203d94c7efa407b15bfec37e25
Daniel Rafail <danielraf2@gmail.com>
2022-09-26 20:31:39 -0400

    main with all the card tests

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,56 @@
 #include "Cards/CardsDriver.h"
 #include "GameEngine/GameEngine.h"
 
+class CardsTester;
+
 // TODO Test functions
 int main() {
-    auto ge = new GameEngine("assets/Moon.map");
+//    auto ge = new GameEngine("assets/Moon.map");
+//
+//    auto bombCard = new BombCard();
+//
+//    auto territory = ge->map->findById(0);
+//
+//    ge->players.push_back(new Player("Bob"));
+//
+//    std::cout << "Armies of " + territory->getName() + " before: " << territory->getArmies() << std::endl;
+//
+//    bombCard->play(ge->players[0]);
+//
+//    ge->executeOrders();
+//
+//    std::cout << "Armies of " + territory->getName() + " after: " << territory->getArmies() << std::endl;
 
-    auto bombCard = new BombCard();
+    auto divider = "================================================================";
+    /* ---------------------------- Testing All Methods ---------------------------- */
+    std::cout << divider << std::endl;
+    std::cout << "Testing Cards.cpp functions" << std::endl;
+    std::cout << divider << std::endl;
+    CardsTester::testBombCardConstructor();
+    CardsTester::testBombCardPlay();
+    CardsTester::testBlockadeCardConstructor();
+    CardsTester::testBlockadeCardPlay();
+    CardsTester::testAirliftCardConstructor();
+    CardsTester::testAirliftCardPlay();
+    CardsTester::testNegotiateCardConstructor();
+    CardsTester::testNegotiateCardPlay();
+    CardsTester::testHandConstructor();
+    CardsTester::testListHand();
+    CardsTester::testHandRemove();
+    CardsTester::testDeckConstructor();
+    CardsTester::testDeckDraw();
+    CardsTester::testDeckPut();
+    CardsTester::testDeckToString();
+    CardsTester::testHandToString();
+    CardsTester::testBombCardToString();
+    CardsTester::testBlockadeCardToString();
+    CardsTester::testAirliftCardToString();
+    CardsTester::testNegotiateCardToString();
 
-    auto territory = ge->map->findById(0);
-
-    ge->players.push_back(new Player("Bob"));
-
-    std::cout << "Armies of " + territory->getName() + " before: " << territory->getArmies() << std::endl;
-
-    bombCard->play(ge->players[0]);
-
-    ge->executeOrders();
-
-    std::cout << "Armies of " + territory->getName() + " after: " << territory->getArmies() << std::endl;
+    /* ---------------------------- Testing TestCards Method ---------------------------- */
+    std::cout << divider << std::endl;
+    std::cout << "Cards Driver : TestCards method" << std::endl;
+    std::cout << divider << std::endl;
+    testCards();
 
 }
