e5a629aa3a8b0b0b8ded5e4ad06afb0bf644e381
Khaled Al K <57811573+kkodmani@users.noreply.github.com>
2022-11-02 15:33:04 -0400

    minor edit in driver

--- src/CommandProcessor/CommandProcessorDriver.cpp
+++ src/CommandProcessor/CommandProcessorDriver.cpp
@@ -4,17 +4,19 @@
 #include "../GameEngine/GameEngine.h"
 
 void testCommandProcessor() {
-  auto ge = new GameEngine();
-  ge->setState(GameEngine::START);
-  auto *cp = new CommandProcessor();
-  for (int i = 0; i < 3; i++) {
-      cp->getCommand();
-  }
+    auto ge = new GameEngine();
+    ge->setState(GameEngine::MAP_LOADED);
+    cout << "Type in 3 commands to test.\nYou are currently in state " << GameEngine::toString(ge->getState())
+         << endl;
+    auto *cp = new CommandProcessor();
+    for (int i = 0; i < 3; i++) {
+        cp->getCommand();
+    }
 
-  auto list = cp->getCommandList();
+    auto list = cp->getCommandList();
 
-  for (auto i : list) {
-    std::cout << *i;
-    cp->validate(i);
-  }
+    for (auto i: list) {
+        std::cout << *i;
+        cp->validate(i);
+    }
 }
