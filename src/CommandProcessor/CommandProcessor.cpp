413d533dec1272eee60532a6a159c1cfc9551fde
Khaled Al K <57811573+kkodmani@users.noreply.github.com>
2022-11-01 21:52:21 -0400

    Started the cp
    basics with just the constructors and operators

--- src/CommandProcessor/CommandProcessor.cpp
+++ src/CommandProcessor/CommandProcessor.cpp
@@ -1,74 +1,148 @@
-//
-// Created by Legu on 2022-10-17.
-//
-
 #include "CommandProcessor.h"
+
+#include <utility>
 #include "../Utils/Utils.h"
 
+
+/**
+ * Command constructor
+ */
+Command::Command() = default;
+
+/**
+ *  Command destructor
+ */
+Command::~Command() = default;
+
+
+/**
+ * Copy constructor for command
+ * @param command name of the command
+ */
+Command::Command(const Command &command) {
+    this->command = command.command;
+    this->arg = command.arg;
+    this->effect = command.effect;
+}
+
+/**
+ * Overload for the = operator on command
+ * @param c command we are assigning to
+ * @return Copy constructor used to create new command
+ */
+Command &Command::operator=(const Command &c) {
+    this->command = c.command;
+    this->effect = c.effect;
+    return *this;
+}
+
+/**
+ * Insertion operator for command class
+ * @param os stream operator
+ * @param c command class
+ * @return information about the command
+ */
+ostream &operator<<(ostream &os, const Command &c) {
+    return cout << "the name of the command is " << c.command << endl
+                << ", and the effect of the command is " << c.effect << endl
+                << ", it's argument is " << c.arg << endl;
+}
+
 Command *CommandProcessor::getCommand() {
-  auto command = readCommand();
+    auto command = readCommand();
 
-  saveCommand(command);
+    saveCommand(command);
 
-  return command;
+    return command;
 }
 
 Command *CommandProcessor::readCommand() {
-  string line;
-  while (true) {
-    line = Utils::getInputString();
+    string line;
+    while (true) {
+        line = Utils::getInputString();
 
-    auto tokens = Utils::tokenizer(line, ' ');
+        auto tokens = Utils::tokenizer(line, ' ');
 
-    if (tokens.empty()) {
-      cout << "You need to input something" << endl;
-      continue;
+        if (tokens.empty()) {
+            cout << "You need to input something" << endl;
+            continue;
+        }
+
+        if (tokens[0] == "loadmap" || tokens[0] == "validatemap" || tokens[0] == "addplayer" ||
+            tokens[0] == "gamestart" || tokens[0] == "replay" || tokens[0] == "quit") {
+            if (tokens.size() == 1) {
+                cout << "This command is missing an argument." << endl;
+            }
+            auto command = new Command();
+            command->command = Utils::trim(tokens[0]);
+            command->arg = Utils::trim(line.substr(tokens[0].length()));
+            return command;
+        } else {
+            auto command = new Command();
+
+            command->command = Utils::trim(line);
+
+            return command;
+        }
     }
+}
 
-    if (tokens[0] == "loadmap" || tokens[0] == "addplayer") {
-      if (tokens.size() == 1) {
-        cout << "This command is missing an argument." << endl;
-      }
-      auto command = new Command();
-      command->command = Utils::trim(tokens[0]);
-      command->arg = Utils::trim(line.substr(tokens[0].length()));
-      return command;
-    } else {
-      auto command = new Command();
+CommandProcessor::CommandProcessor() = default;
 
-      command->command = Utils::trim(line);
+CommandProcessor::CommandProcessor(const CommandProcessor &commandProcessor) {
+    this->commands = commandProcessor.commands;
+}
 
-      return command;
-    }
-  }
+CommandProcessor::~CommandProcessor() = default;
+
+CommandProcessor &CommandProcessor::operator=(const CommandProcessor &commandProcessor) = default;
+
+ostream &operator<<(ostream &os, const CommandProcessor &commandProcessor) {
+    // TODO format this
+     os << "list of commands: \n" ;
+
+     for (auto i: commandProcessor.commands) {
+         os << i->getCommand() << endl;
+     }
+     return os;
 }
 
 void CommandProcessor::saveCommand(Command *command) {
-  commands.push_back(command);
+    commands.push_back(command);
 }
 
 const string &Command::getCommand() const {
-  return command;
+    return command;
 }
+
 const string &Command::getArg() const {
-  return arg;
+    return arg;
 }
+
 const string &Command::getEffect() const {
-  return effect;
+    return effect;
 }
-void Command::setEffect(const string &Effect) {
-  effect = Effect;
+
+void Command::saveEffect(const string &Effect) {
+    effect = Effect;
 }
 
 bool Command::operator==(const string &rhs) const {
-  return Utils::isEqualLowercase(command, rhs);
+    return Utils::isEqualLowercase(command, rhs);
 }
 
 bool Command::operator!=(const string &rhs) const {
-  return !Utils::isEqualLowercase(command, rhs);
+    return !Utils::isEqualLowercase(command, rhs);
 }
 
 Command *CommandProcessor::getCommand(const string &prompt) {
-  cout << prompt << endl;
-  return getCommand();
+    cout << prompt << endl;
+    return getCommand();
 }
+
+
+
+
+
+
+
--- src/CommandProcessor/CommandProcessor.h
+++ src/CommandProcessor/CommandProcessor.h
@@ -5,43 +5,97 @@
 #include <vector>
 #include <memory>
 #include <iostream>
+#include <fstream>
+#include "../GameEngine/GameEngine.h"
+
 using std::string;
 using std::vector;
 using std::cout;
 using std::endl;
+using std::ifstream;
+using std::streampos;
+using std::ostream;
 
 class CommandProcessor;
 
+class GameEngine;
+
+// Command class
 class Command {
 public:
-  [[nodiscard]] const string &getCommand() const;
-  [[nodiscard]] const string &getArg() const;
-  [[nodiscard]] const string &getEffect() const;
+    Command();
 
-  bool operator!=(const string &rhs) const;
-  bool operator==(const string &rhs) const;
+    Command(const Command &);
 
-  void setEffect(const string &Effect);
+    virtual ~Command();
+
+    Command &operator=(const Command &);
+
+    friend ostream &operator<<(ostream &, const Command &);
+
+    [[nodiscard]] const string &getCommand() const;
+
+    [[nodiscard]] const string &getArg() const;
+
+    [[nodiscard]] const string &getEffect() const;
+
+    bool operator!=(const string &rhs) const;
+
+    bool operator==(const string &rhs) const;
+
+    void saveEffect(const string &Effect);
 
 private:
-  string command;
-  string arg;
-  string effect;
+    string command;
+    string arg;
+    string effect;
 
-  friend class CommandProcessor;
+    friend class CommandProcessor;
 };
 
+// Command processor class
+
 class CommandProcessor {
-  Command *getCommand();
-
 public:
-  [[nodiscard]] Command * getCommand(const string &prompt);
+
+    CommandProcessor();
+
+    CommandProcessor(const CommandProcessor &);
+
+    virtual ~CommandProcessor();
+
+    CommandProcessor &operator=(const CommandProcessor &);
+
+    friend ostream &operator<<(ostream &, const CommandProcessor &);
+
+    bool validate(GameEngine* , Command* );
+
+    Command *getCommand();
+
+    [[nodiscard]] Command *getCommand(const string &prompt);
+
 private:
-  static Command *readCommand();
 
-  void saveCommand(Command *command);
+    static Command *readCommand();
 
-  vector<Command *> commands;
+    void saveCommand(Command *command);
+
+    vector<Command *> commands;
+
+    friend class FileCommandProcessorAdapter;
+};
+
+// File line reader
+
+class FileLineReader  {
+    // TODO implement
+};
+
+
+// File command processor class
+
+class FileCommandProcessorAdapter : public CommandProcessor {
+    // TODO implement
 };
 
 #endif //WARZONE_SRC_COMMANDPROCESSOR_COMMANDPROCESSOR_H
